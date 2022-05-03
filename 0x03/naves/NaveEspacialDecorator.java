f3df2a39bda3994441ceaa4b24c104af7564c748
LucasGves <lucasqueiroz@ciandt.com>
2022-05-03 20:06:12 -0300

    Guerra de Naves Espaciais

---
+++ 0x03/naves/NaveEspacialDecorator.java
@@ -0,0 +1,18 @@
+public abstract class NaveEspacialDecorator extends NaveEspacial{
+    private NaveEspacial naveEspacial;
+
+    public NaveEspacialDecorator(NaveEspacial naveEspacial) {
+        this.naveEspacial = naveEspacial;
+    }
+
+
+    @Override
+    public int getSaude() {
+        return naveEspacial.getSaude();
+    }
+
+    @Override
+    public int getAtaque() {
+        return naveEspacial.getAtaque();
+    }
+}
